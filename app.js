89bdb15a23f4487c8ba9a5279e1a7301d22c4b26
unknown <Loop@mum-sachin.loopmobile>
2020-01-15 18:55:02 +0530

    Setup app for Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 
-const port =proccess.env.PORT ||3000
+const port =process.env.PORT ||3000
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
